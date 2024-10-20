a8dc306cc3ccb9aa9658b8ce6338e5f2b4403fc1
Dipen <dipendraboyaju222@gmail.com>
2024-10-20 13:16:02 +0545

    change

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -15,6 +15,7 @@ const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
     origin: 'https://buzzchat-two.vercel.app',
+    methods: ['GET', 'POST'],
     credentials: true
   }
 })
